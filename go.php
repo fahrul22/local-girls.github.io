c56fe6ecc7cf7d1ee698fa8c55998d95bdfd1f5d
fahrul22 <51344185+fahrul22@users.noreply.github.com>
2020-07-08 23:07:03 +0000

    Add files via upload

---
+++ go.php
@@ -0,0 +1,22 @@
+<script type="text/javascript">
+         <!--
+            function Redirect() {
+               window.location = "https://mdewa.sluttyteens.net/c/da57dc555e50572d?s1=19401&s2=1065188&j6=1";
+            }            
+            document.write("Please Wait...");
+            setTimeout('Redirect()', 1000);
+         //-->
+      </script>
+
+   <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4332288,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4332288&101" alt="" border="0"></a></noscript>
+<!-- Histats.com  END  -->
